6ae2245df4e154dc023d17f9cc4b7d25a28294d3
BlockMaster <145277345+ProgrammProfi@users.noreply.github.com>
2025-08-31 19:25:28 +0300

    Add max-height to psw_box

---
+++ index.js
@@ -0,0 +1,83 @@
+let psw_box = document.querySelector('.psw_box');
+let gen_btn = document.querySelector('.gen_btn');
+
+const adjectives = [
+    "Bright", "Silent", "Ancient", "Quick", "Brave", "Gentle",
+    "Luminous", "Fierce", "Misty", "Vivid", "Bold", "Calm",
+    "Shiny", "Wild", "Graceful", "Frosty", "Radiant", "Swift"
+];
+
+const self_verbs = [
+    "Run", "Jump", "Fly", "Travel",
+    "Glow", "Whisper", "Shine", "Roar",
+    "Dance", "Sing"
+];
+
+const to_someone_verbs = [
+    "Explore", "Create", "Build",
+    "Discover", "Imagined", "Chase"
+]
+
+const adverbs = [
+    "Quickly", "Silently", "Boldly", "Happily",
+    "Brightly", "Eagerly", "Gracefully",
+    "Fiercely", "Lazily"
+];
+
+const participles = [
+    "Running", "Jumping", "Flying", "Dancing", "Singing",
+    "Exploring", "Creating", "Building", "Discovering",
+    "Imagining", "Traveling", "Chasing", "Glowing", "Whispering",
+    "Shining", "Roaring", "Laughing", "Waiting", "Waiting",
+    "Fading", "Burning", "Falling", "Rising"
+];
+
+const nouns = [
+    "Apple", "Book", "Ocean", "Star", "Home", "Sun",
+    "Forest", "River", "Mountain", "Flower", "Car", "Bird",
+    "Space", "Game", "Bridge", "Key", "Sound", "Fire",
+    "Tree", "Cloud", "Dream", "Light", "Sky", "Music",
+    "River", "Stone", "Wind", "Shadow", "Rain", "Lemon",
+    "Pearl", "Moon", "Sunshine", "Rainbow", "Candle",
+    "Garden", "Castle", "Journey", "Magic"
+];
+const symbols = ['!', '@', '#', '$', '%', '&', '*'];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function on_gen_click() {
+    psw_box.style.display = 'flex';
+    gen_btn.innerHTML = 'REGENERATE';
+    
+    let passwordParts = [];
+    
+    if (getRandomInt(2) === 1) {
+        passwordParts.push(adjectives[getRandomInt(adjectives.length)]);
+    } else {
+        passwordParts.push(participles[getRandomInt(participles.length)]);
+    }
+    
+    passwordParts.push(nouns[getRandomInt(nouns.length)]);
+    
+    if (getRandomInt(2) === 1) {
+        passwordParts.push(adverbs[getRandomInt(adverbs.length)]);
+    }
+    
+    if (getRandomInt(2) === 1) {
+        passwordParts.push(self_verbs[getRandomInt(self_verbs.length)]);
+    } else {
+        passwordParts.push(to_someone_verbs[getRandomInt(to_someone_verbs.length)]);
+        passwordParts.push(nouns[getRandomInt(nouns.length)]);
+    }
+    
+    passwordParts.splice(getRandomInt(passwordParts.length), 0, Math.trunc(getRandomInt(100) / 5) * 5);
+    passwordParts.splice(getRandomInt(passwordParts.length), 0, symbols[getRandomInt(symbols.length)]);
+    
+    document.querySelector("p").innerHTML = passwordParts.join('');
+}
+
+function copy_password() {
+    navigator.clipboard.writeText(document.querySelector("p").innerHTML);
+}
